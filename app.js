d25c54339489c4a3140846fb40c27c09e8d6cbe5
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-07-15 14:58:47 -0500

    add package.json and setup to use ES syntax; import log func from logger module into app module

---
+++ app.js
@@ -0,0 +1,3 @@
+import { log as logIt } from './logger.js'
+
+logIt('Hello World')
--- logger.js
+++ logger.js
@@ -1,9 +1,7 @@
 // HTTP URL endpoint
 const url = 'http://logger.io/log';
 
-function log(message) {
+export function log(message) {
   // Send HTTP req
   console.log(message)
 }
-
-module.exports.log = log
