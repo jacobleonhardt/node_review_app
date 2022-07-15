16f18a2eefa03a960409f8249f6d8f153015e009
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-07-15 16:03:29 -0500

    add event listeners; emit events and handle

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-import { log as logIt } from './logger.js'
+import { log } from './logger.js'
+import { EventEmitter } from 'events'
 
-logIt('Hello World')
+const customEvent = new EventEmitter()
+const loggedMessage = log('Hello World')
+// Regist a listener that will be called when event is raised
+// .addListener and .on are pretty much the same thing; or achieve the same goal
+customEvent.on('eventOccured', (e) => {
+  console.log('Event: ', e)
+})
+
+customEvent.on('logger', (e) => {
+  console.log('Log: ', e.message)
+})
+
+// Use .emit to raise an event -- signal something has happened
+// .emit must be called AFTER .on/.addListener -- emit itterates over all registered listeners and calls them synchronusly
+customEvent.emit('eventOccured', { id: 1, url: 'http://', })
+customEvent.emit('logger', { message: 'Hello World' })
--- logger.js
+++ logger.js
@@ -1,7 +1,10 @@
 // HTTP URL endpoint
 const url = 'http://logger.io/log';
 
-export function log(message) {
+function log(message) {
   // Send HTTP req
   console.log(message)
 }
+
+export { log }
+ 
